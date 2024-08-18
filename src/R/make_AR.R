669e20d5812c79aa151aed3378ad063592d3f4e0
Philip ks <3000piksel@gmail.com>
2024-08-18 19:28:23 +0200

    split make AR & MA processes

--- src/R/make_AR.R
+++ src/R/make_AR.R
@@ -4,9 +4,9 @@
 #'
 #' @details
 #' The AR(p) model can be defined by the equation
-#' 
+#'
 #' \deqn{X_t = \phi_1 X_{t-1} + \phi_2 X_{t-2} + \dots + \phi_p X_{t-p} + Z_t,}
-#' 
+#'
 #' where \eqn{X_t} represents the time series at time \eqn{t} and \eqn{Z_t} is white noise with a mean of zero and a variance of \eqn{\sigma^2}.
 #'
 #' @param t An integer specifying the desired length of the time series.
@@ -58,7 +58,7 @@ make_ar <- function(t, phi, sigma = 1, start = numeric(length(phi))) {
 
   p <- length(phi)
   X <- c(start, numeric(t - length(start)))
-  Z <- rnorm(t, mean = 0, sd = sigma)
+  Z <- stats::rnorm(t, mean = 0, sd = sigma)
 
   for (i in (p + 1):t) {
     X[i] <- sum(phi * X[(i - 1):(i - p)]) + Z[i]
@@ -67,62 +67,3 @@ make_ar <- function(t, phi, sigma = 1, start = numeric(length(phi))) {
   return(X)
 }
 
-#' @title Generator for MA(q) processes
-#'
-#' @description `make_ma` generates a synthetic time series that follows an MA(q) model.
-#'
-#' @details
-#' The MA(q) model can be defined by the equation
-#' 
-#' \deqn{X_t = Z_t + \theta_1 Z_{t-1} + \theta_2 Z_{t-2} + \dots + \theta_q Z_{t-q},}
-#' 
-#' where \eqn{X_t} represents the time series at time \eqn{t} and \eqn{Z_t} is white noise with a mean of zero and variance of \eqn{\sigma^2}.
-#'
-#' @param t An integer specifying the desired length of the time series.
-#'
-#' @param theta A numeric or complex atomic vector of length `q` containing the coefficients of the MA(q) process.
-#'
-#' @param sigma A numeric value representing the variance of the white noise, with a default value of 1.
-#'
-#' @returns A numeric atomic vector containing the generated MA(q) time series.
-#'
-#' @references Brockwell, P.J., Davis, R.A. (2016) \emph{Introduction to Time Series and Forecasting}. Springer.
-#'
-#' @examples
-#' # Simple AR(1) model
-#' ma_one <- make_ma(100, -0.25)
-#' ts.plot(ma_one)
-#' acf(ma_one)
-#'
-#' @export
-make_ma <- function(t, theta, sigma = 1) {
-  stopifnot(
-    "t must be a value of length 1" = (is.atomic(t) & length(t) == 1),
-    "t must not be infinite" = !is.infinite(t),
-    "t must not be NA" = !is.na(t),
-    "t must be numeric" = is.numeric(t),
-    "t must be an integer" = t %% 1 == 0,
-    "t must be positive" = t > 0,
-    "theta must be an atomic vector" = is.atomic(theta),
-    "theta must have positive length" = length(theta) > 0,
-    "theta may not contain NAs" = !any(is.na(theta)),
-    "theta may not contain Inf or -Inf values" = !any(is.infinite(theta)),
-    "The values of theta must be numeric or complex" = (is.numeric(theta) | is.complex(theta)),
-    "t must be greater than the length of theta" = t > length(theta),
-    "sigma must be a value of length 1" = (is.atomic(sigma) & length(sigma) == 1),
-    "sigma must be finite" = !is.infinite(sigma),
-    "sigma must not be NA" = !is.na(sigma),
-    "sigma must be numeric" = is.numeric(sigma),
-    "sigma must be positive" = sigma > 0
-  )
-
-  q <- length(theta)
-  X <- numeric(t)
-  Z <- rnorm(t + q, mean = 0, sd = sigma)
-
-  for (i in (q + 1):(t + q)) {
-    X[i - q] <- sum(theta * Z[(i - 1):(i - q)]) + Z[i]
-  }
-
-  return(X)
-}
---
+++ src/R/make_MA.R
@@ -0,0 +1,60 @@
+#' @title Generator for MA(q) processes
+#'
+#' @description `make_ma` generates a synthetic time series that follows an MA(q) model.
+#'
+#' @details
+#' The MA(q) model can be defined by the equation
+#'
+#' \deqn{X_t = Z_t + \theta_1 Z_{t-1} + \theta_2 Z_{t-2} + \dots + \theta_q Z_{t-q},}
+#'
+#' where \eqn{X_t} represents the time series at time \eqn{t} and \eqn{Z_t} is white noise with a mean of zero and variance of \eqn{\sigma^2}.
+#'
+#' @param t An integer specifying the desired length of the time series.
+#'
+#' @param theta A numeric or complex atomic vector of length `q` containing the coefficients of the MA(q) process.
+#'
+#' @param sigma A numeric value representing the variance of the white noise, with a default value of 1.
+#'
+#' @returns A numeric atomic vector containing the generated MA(q) time series.
+#'
+#' @references Brockwell, P.J., Davis, R.A. (2016) \emph{Introduction to Time Series and Forecasting}. Springer.
+#'
+#' @examples
+#' # Simple AR(1) model
+#' ma_one <- make_ma(100, -0.25)
+#' ts.plot(ma_one)
+#' acf(ma_one)
+#'
+#' @export
+make_ma <- function(t, theta, sigma = 1) {
+  stopifnot(
+    "t must be a value of length 1" = (is.atomic(t) & length(t) == 1),
+    "t must not be infinite" = !is.infinite(t),
+    "t must not be NA" = !is.na(t),
+    "t must be numeric" = is.numeric(t),
+    "t must be an integer" = t %% 1 == 0,
+    "t must be positive" = t > 0,
+    "theta must be an atomic vector" = is.atomic(theta),
+    "theta must have positive length" = length(theta) > 0,
+    "theta may not contain NAs" = !any(is.na(theta)),
+    "theta may not contain Inf or -Inf values" = !any(is.infinite(theta)),
+    "The values of theta must be numeric or complex" = (is.numeric(theta) | is.complex(theta)),
+    "t must be greater than the length of theta" = t > length(theta),
+    "sigma must be a value of length 1" = (is.atomic(sigma) & length(sigma) == 1),
+    "sigma must be finite" = !is.infinite(sigma),
+    "sigma must not be NA" = !is.na(sigma),
+    "sigma must be numeric" = is.numeric(sigma),
+    "sigma must be positive" = sigma > 0
+  )
+
+  q <- length(theta)
+  X <- numeric(t)
+  Z <- stats::rnorm(t + q, mean = 0, sd = sigma)
+
+  for (i in (q + 1):(t + q)) {
+    X[i - q] <- sum(theta * Z[(i - 1):(i - q)]) + Z[i]
+  }
+
+  return(X)
+}
+
