809a668eed8d568af4a326c7d58b513cc0265e0a
Philip ks <3000piksel@gmail.com>
2024-08-18 18:23:01 +0200

    fix documentation errors

--- src/R/plot.R
+++ src/R/plot.R
@@ -6,8 +6,7 @@
 #' This function is designed to visualize a time series, where the last `h` values are considered predictions and are highlighted in a different color. The main part of the time series is plotted in blue, while the predicted values are plotted in red. The function automatically generates a legend to distinguish between the observed and predicted values.
 #'
 #' @param X A numeric vector representing the time series data, including the predictions.
-#'
-#' @param h An integer specifying the number of predictions to be highlighted in the plot. The value of `h` must be a positive integer and should not exceed the length of `X_hat`.
+#' @param h an integer specifying the number of predictions to be highlighted in the plot. The value of `h` must be a positive integer and should not exceed the length of `X_hat`.
 #'
 #' @return A line plot displaying the time series with the last `h` values highlighted as predictions in red.
 #'
@@ -21,13 +20,13 @@
 #' @export
 plot <- function(X, h) {
   stopifnot(
-    "h is not a numeric integer" = n == as.integer(h),
-    "h cannot be greater than the length of X_hat" = h <= length(X)
+    "h is not a numeric integer" = h %% 1 == 0,
+    "h cannot be greater than the length of X" = h <= length(X)
   )
 
   P <- (length(X) - h + 1):length(X)
 
-  plot(1:length(X), X, type = "l", col = "blue", xlab = "Index", ylab = "Value", main = "Custom Line Plot")
-  lines(P, X[P], col = "red")
-  legend("topright", legend = c("X", "P"), col = c("blue", "red"), lty = 1)
+  graphics::plot(1:length(X), X, type = "l", col = "blue", xlab = "Index", ylab = "Value", main = "Custom Line Plot")
+  graphics::lines(P, X[P], col = "red")
+  graphics::legend("topright", legend = c("X", "P"), col = c("blue", "red"), lty = 1)
 }
