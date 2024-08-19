eae45cd272914e3119d751eafabedba7aea1ea2c
Philip ks <3000piksel@gmail.com>
2024-08-19 14:11:00 +0200

    change plot() documentation

--- src/R/plot.R
+++ src/R/plot.R
@@ -1,12 +1,12 @@
-#' @title Custom Time Series Plotting Function
+#' @title Custom Spectral Density Plotting Function
 #'
-#' @description `plot` generates a custom line plot of a time series vector, highlighting the last `h` values as predictions. The plot displays the time series in blue and the predictions in red, with an appropriate legend.
+#' @description `plot` computes periodogram values, plots the spectral density, and includes a legend. It samples over \eqn{(-\pi, \pi]} and manages errors.
 #'
 #' @details
-#' This function is designed to visualize a time series, where the last `h` values are considered predictions and are highlighted in a different color. The main part of the time series is plotted in blue, while the predicted values are plotted in red. The function automatically generates a legend to distinguish between the observed and predicted values.
+#' The function computes periodogram values using the `periodogram` function from the `stats` package. It then generates a plot of the spectral density estimate with the `plot` function from the `graphics` package, including a legend to distinguish between observed and predicted values. The samples are evaluated at evenly spaced points over the interval \eqn{(-\pi, \pi]}. Additionally, the function incorporates error handling to manage issues such as invalid inputs or missing packages.
 #'
 #' @param X A numeric vector representing the time series data, including the predictions.
-#' @param samples an integer specifying the number of samples to evaluate the periodogram with.
+#' @param n an integer specifying the number of samples to evaluate the periodogram with.
 #'
 #' @return A line plot displaying the spectral density estimate of the time series.
 #'
@@ -18,7 +18,7 @@
 #' plot(X, 100)
 #'
 #' @export
-plot <- function(X, samples) {
+plot <- function(X, n) {
   stopifnot(
     "X must be an atomic vector" = is.atomic(X),
     "X must have positive length" = length(X) > 0,
@@ -26,15 +26,15 @@ plot <- function(X, samples) {
     "X may not contain Inf or -Inf values" = !any(is.infinite(X)),
     "X must be numeric or complex" = (is.numeric(X) | is.complex(X)),
     "X may not contain Inf or -Inf values" = !any(is.infinite(X)),
-    "samples must be numeric" = is.numeric(samples),
-    "samples must be an integer" = samples %% 1 == 0 & length(samples) == 1
+    "samples must be numeric" = is.numeric(n),
+    "samples must be an integer" = n %% 1 == 0 & length(n) == 1
   )
 
-  lambdas <- seq(from = -pi, to = pi, length.out = samples + 1)[-1]
+  lambda <- seq(from = -pi, to = pi, length.out = n + 1)[-1]
 
-  I <- sapply(lambdas, \(i) {
+  samples <- sapply(lambda, \(i) {
     periodogram(X, i)
   })
 
-  graphics::plot(1:length(I), I, type = "l", col = "blue", xlab = "Index", ylab = "Value", main = "The spectral density estimate of X")
+  graphics::plot(1:(n+1), samples, type = "l", col = "blue", xlab = "Index", ylab = "Value", main = "The spectral density estimate of X")
 }
