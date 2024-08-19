a635c7a60764d23dcd5c5951ed0ad6c8188fe332
Philip ks <3000piksel@gmail.com>
2024-08-19 14:17:32 +0200

    minor refactorings on plot()

--- src/R/plot.R
+++ src/R/plot.R
@@ -12,10 +12,10 @@
 #'
 #' @examples
 #' # Generate a random time series with 100 elements
-#' X <- rnorm(100)
+#' X <- rnorm(500)
 #'
-#' # Plot the time series with the last 10 elements highlighted as predictions
-#' plot(X, 100)
+#' # Plot the spectral density
+#' zeitreihen::plot(X, 100)
 #'
 #' @export
 plot <- function(X, n) {
@@ -26,8 +26,8 @@ plot <- function(X, n) {
     "X may not contain Inf or -Inf values" = !any(is.infinite(X)),
     "X must be numeric or complex" = (is.numeric(X) | is.complex(X)),
     "X may not contain Inf or -Inf values" = !any(is.infinite(X)),
-    "samples must be numeric" = is.numeric(n),
-    "samples must be an integer" = n %% 1 == 0 & length(n) == 1
+    "n must be numeric" = is.numeric(n),
+    "n must be an integer" = n %% 1 == 0 & length(n) == 1
   )
 
   lambda <- seq(from = -pi, to = pi, length.out = n + 1)[-1]
@@ -36,5 +36,5 @@ plot <- function(X, n) {
     periodogram(X, i)
   })
 
-  graphics::plot(1:(n+1), samples, type = "l", col = "blue", xlab = "Index", ylab = "Value", main = "The spectral density estimate of X")
+  graphics::plot(1:n, samples, type = "l", col = "blue", xlab = "Index", ylab = "Value", main = "The spectral density estimate of X")
 }
--- src/tests/testthat/test_plot.R
+++ src/tests/testthat/test_plot.R
@@ -2,28 +2,28 @@
 ## Test conditions on X
 
 test_that("TEST plot: conditions on X", {
-  samples <- 100
+  n <- 100
 
   X <- list(1, 2, 3, 4)
-  expect_error(plot(X, samples), "X must be an atomic vector")
+  expect_error(plot(X, n), "X must be an atomic vector")
 
   X <- numeric(0)
-  expect_error(plot(X, samples), "X must have positive length")
+  expect_error(plot(X, n), "X must have positive length")
 
   X <- c(1, 2, NA, 3, 4)
-  expect_error(plot(X, samples), "X may not contain NAs")
+  expect_error(plot(X, n), "X may not contain NAs")
 
   X <- c(1, 2, Inf, 3, 4)
-  expect_error(plot(X, samples), "X may not contain Inf or -Inf values")
+  expect_error(plot(X, n), "X may not contain Inf or -Inf values")
 
   X <- c("s", "s", "t", "s")
-  expect_error(plot(X, samples), "X must be numeric or complex")
+  expect_error(plot(X, n), "X must be numeric or complex")
 
-  samples <- "s"
+  n <- "s"
   X <- c(1, 2, 3, 4)
-  expect_error(plot(X, samples), "samples must be numeric")
+  expect_error(plot(X, n), "n must be numeric")
 
-  samples <- 3.3
+  n <- 3.3
   X <- c(1, 2, 3, 4)
-  expect_error(plot(X, samples), "samples must be an integer")
+  expect_error(plot(X, n), "n must be an integer")
 })
